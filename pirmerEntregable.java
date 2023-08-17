0288433a0f4d35f9b7e83265a178e0cf8ff313fd
analistaRoberto <analistaroberto.pena@gmail.com>
2023-08-16 23:32:24 -0400

    Primer Entregable Java

---
+++ pirmerEntregable.java
@@ -0,0 +1,15 @@
+ class main{
+
+public static void main(String[] args) 
+    {
+    int  numeroInt = 20;
+    float numeroFloat= numeroInt;
+    
+    System.out.println(numeroInt);
+    System.out.println(numeroFloat);
+   
+    }
+}
+
+
+
